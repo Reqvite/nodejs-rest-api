6ff83d24dcb10680841e6ba47892b6b641e63217
Reqvite <reqvite2233@gmail.com>
2023-01-28 18:42:10 +0200

    add test

--- controllers/authController.js
+++ controllers/authController.js
@@ -10,7 +10,6 @@ const {
 const registrationController = async (req, res) => {
   const { email, password } = req.body;
   const user = await registration(email, password);
-
   res.status(201).json({ status: "Created", code: 201, user });
 };
 
